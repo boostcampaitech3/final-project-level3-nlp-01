9e91ec51de1af815930d5a162b16cb2a75f8e78e
leeyejin1231 <ssgyejin@g.skku.edu>
2022-05-25 18:33:39 +0000

    feat: make crawler

---
+++ src/crawler.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+import pandas as pd
+
+class MusicLyricsCrawler:
+    def __init__(self) -> None:
+        pass
+    
+    def crawl(self, num):
+        data = []
+
+        music_url = "https://www.melon.com/chart/index.htm"
+
+        print('Initialize Drive')
+        driver = self.initDrive(music_url)
+
+        print('Start parsing')
+        data = self.parse(driver, num)
+
+        print('data 수:', len(data))
+
+        print('Save to csv')
+        data.to_csv(f"Lyrics top{num}.csv", encoding='utf-8')
+
+        return data
+
+    def initDrive(self, music_url):
+        chrome_options = webdriver.ChromeOptions()
+
+        chrome_options.add_argument('--headless') #내부 창을 띄울 수 없으므로 설정
+
+        chrome_options.add_argument('--no-sandbox')
+
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+        driver = webdriver.Chrome('chromedriver.exe')
+        driver.get(music_url)
+
+        return driver
+
+    def parse(self, driver, num):
+        titles = driver.find_elements_by_class_name('ellipsis.rank01')
+        titles2 = []
+        for i in titles:
+            titles2.append(i.text)
+        
+        del titles2[0]
+        del titles2[num:]
+        
+        singers = driver.find_elements_by_class_name('ellipsis.rank02')
+        singers2 = []
+        for i in singers:
+            singers2.append(i.text)
+        
+        del singers2[0]
+        del singers2[num:]
+
+        songTagList = driver.find_elements_by_id('lst50')
+        number = []
+        for i in songTagList:
+            number.append(i.get_attribute('data-song-no'))
+
+        Lyric = []
+        for i in number:
+            if i % 10 == 0:
+                print('진행중 : ', i, "번 째")
+            driver.get("https://www.melon.com/song/detail.htm?songId=" + i)
+            lyric = driver.find_element_by_class_name('lyric')
+            Lyric.append(lyric.text)
+
+        data = pd.DataFrame({"title":titles2, "singer":singers2, "lyric":Lyric})
+
+        return data
