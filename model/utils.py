81b9d8bbfce85a33afbdc6403910a9aac575f1ab
Kang-Beom-Seo <bumsur@hanmail.net>
2022-05-29 03:46:47 +0000

    text cleaning added

---
+++ model/utils.py
@@ -0,0 +1,15 @@
+import re
+import emoji
+from soynlp.normalizer import repeat_normalize
+
+emojis = ''.join(emoji.UNICODE_EMOJI.keys())
+pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣{emojis}]+')
+url_pattern = re.compile(
+    r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+def clean(x):
+    x = pattern.sub(' ', x)
+    x = url_pattern.sub('', x)
+    x = x.strip()
+    x = repeat_normalize(x, num_repeats=2)
+    return x
