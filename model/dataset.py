be057de5c143d3cfa1dc67bcb3431009a81f1bc3
Kang-Beom-Seo <bumsur@hanmail.net>
2022-06-01 02:09:00 +0000

    dataset bug fixed

--- model/dataset.py
+++ model/dataset.py
@@ -8,8 +8,12 @@ class SongDataset(Dataset):
         tokenized_text = tokenizer(text, return_tensors="pt", padding=True,
                                    truncation=True, max_length=512, add_special_tokens=True)
         self.dataset = tokenized_text # {'input_ids': ~, 'token_type_ids': ~, 'attention_mask': ~, 'entity_ids' : ~}
-        labels = mlb.transform(labels)
-        self.labels = labels # multi-label one-hot encoding
+        transformed_labels = []
+        for label in labels:
+            transformed_label = eval(label)
+            transformed_labels.append(transformed_label)
+        transformed_labels = mlb.transform(transformed_labels)
+        self.labels = transformed_labels # multi-label one-hot encoding
 
     def __getitem__(self, idx):
         item = {key: val[idx].clone().detach() for key, val in self.dataset.items()}
