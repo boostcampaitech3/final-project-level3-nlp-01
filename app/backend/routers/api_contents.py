510696f232f8a268f8aefcbee1104379548a7a66
Kang-Beom-Seo <bumsur@hanmail.net>
2022-06-02 19:14:19 +0000

    api_plays added

--- app/backend/routers/api_contents.py
+++ app/backend/routers/api_contents.py
@@ -30,6 +30,12 @@ class MovieOutput(BaseModel):
     image: str = Field(..., description="영화 이미지 주소")
     preview: str = Field(..., description="영화 소개의 처음 일부분 미리보기")
 
+class PlayOutput(BaseModel):
+    title: str = Field(..., description="연극 제목")
+    hyperlink: str = Field(..., description="연극 사이트 주소")
+    image: str = Field(..., description="연극 이미지 주소")
+    preview: str = Field(..., description="연극 소개의 처음 일부분 미리보기")
+
 router = APIRouter(prefix="/contents")
 
 @router.on_event("startup")
@@ -38,11 +44,12 @@ def startup_event():
     from os.path import join
     from app.backend.main import top_dir
     
-    global songs_database, books_database, movies_database,\
-           playlist, booklist, movielist
+    global songs_database, books_database, movies_database, plays_database,\
+           playlist, booklist, movielist, theaterlist
     playlist = []
     booklist = []
     movielist = []
+    theaterlist = []
     
     songs_database_dir = join(top_dir, "app/database/songs_database.pkl")
     with open(songs_database_dir, 'rb') as f:
@@ -55,6 +62,10 @@ def startup_event():
     movies_database_dir = join(top_dir, "app/database/movies_database.pkl")
     with open(movies_database_dir, 'rb') as f:
         movies_database = pickle.load(f)
+    
+    plays_database_dir = join(top_dir, "app/database/plays_database.pkl")
+    with open(plays_database_dir, 'rb') as f:
+        plays_database = pickle.load(f)
 
 @router.post("/recommend", response_model=DiaryContentInput)
 def recommend_contents(diary_and_feelings: DiaryAndFeelings):
@@ -82,6 +93,13 @@ def recommend_contents(diary_and_feelings: DiaryAndFeelings):
         return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"msg" : "Cannot search movies"})
     movies_content = eval(movies_response.content.decode("UTF-8"))
     recommended_content["movies"] = movies_content
+    
+    # 연극 리스트 가져오기
+    plays_response = requests.post("http://localhost:8000/contents/plays/search", json={"feelings" : feelings})
+    if plays_response.status_code != 200:
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"msg" : "Cannot search plays"})
+    plays_content = eval(plays_response.content.decode("UTF-8"))
+    recommended_content["plays"] = plays_content
     
     output_contents = DiaryContentInput(record_time=diary_and_feelings['record_time'],
                                         diary_content=diary_and_feelings['diary_content'],
@@ -168,3 +186,28 @@ def search_movielist(feelings: FeelingInput):
     movielist = [movies_database[feeling] for feeling in feelings]
     
     return movielist
+
+@router.get('/plays')
+def view_theaterlist():
+    if theaterlist:
+        return theaterlist
+    else:
+        raise HTTPException(status_code=404, detail="아직 오늘의 일기를 받지 못했습니다.")
+
+@router.post("/plays/search", response_model=List[List[PlayOutput]])
+def search_theaterlist(feelings: FeelingInput):
+    """현재의 감정 리스트를 입력으로 넣고, Top-3개의 감정에 해당하는 연극 리스트를 쭉 불러옵니다.
+
+    Args:
+        feelings (FeelingInput): 감정 리스트(ex : ['기쁨', '고마움', '행복'])
+
+    Returns:
+        List[List[PlayOutput]]: 연극 리스트. 이중 리스트로 되어있는데,
+        바깥 쪽의 원소는 각각의 감정 1개의 연극 목록이며,
+        안쪽의 원소는 각각 연극 1개의 정보이다.
+    """
+    feelings = feelings.feelings
+    global plays_database, theaterlist
+    theaterlist = [plays_database[feeling] for feeling in feelings]
+    
+    return theaterlist
