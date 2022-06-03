f4a36ca8dd963bcfdbb48b0cd4123d1ad83083bb
Kang-Beom-Seo <bumsur@hanmail.net>
2022-06-03 02:18:09 +0000

    api_selection_history & api_clear_db added

--- app/backend/routers/api_history.py
+++ app/backend/routers/api_history.py
@@ -12,6 +12,10 @@ class DiaryContentInput(BaseModel):
     feelings: List[str] = Field(..., description="일기에 담긴 감정들")
     recommended_content: Dict[str, List] = Field(..., description="감정에 맞추어 추천받은 콘텐츠들")
 
+class SelectionInput(BaseModel):
+    record_time: str = Field(..., description="콘텐츠가 선택된 날짜")
+    selected_content: Dict[str, str] = Field(..., description="선택된 콘텐츠들")
+
 router = APIRouter(prefix='/history')
 
 @router.on_event("startup")
@@ -56,12 +60,54 @@ def insert_diary_record(insert_form: DiaryContentInput):
     try:
         diary_history_database.appendleft(diary_record)
     except:
-        return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE, content={"msg" : "Cannot append to local database"})
+        return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE, content={"msg" : "Cannot append to local diary database"})
                             
     try:
         with open(diary_history_database_dir, 'wb') as f:
             pickle.dump(diary_history_database, f)
     except:
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"msg" : "Cannot dump database!"})
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"msg" : "Cannot dump history database!"})
     
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"msg" : "Diary History Database Updated!"})
+
+@router.post("/selection/insert")
+def insert_selection_record(insert_form: SelectionInput):
+    """ 선택 기록 1개 {시간, 선택된 컨텐츠}를 selection history에 넣는다.
+
+    Args:
+        insert_form (SelectionInput): 선택 기록 1개 {시간, 선택된 컨텐츠}
+
+    Returns:
+        정상 동작 시 Response 201 반환
+    """
+    selection_record = insert_form.dict()
+    try:
+        selection_history_database.appendleft(selection_record)
+    except:
+        return JSONResponse(status_code=status.HTTP_406_NOT_ACCEPTABLE, content={"msg" : "Cannot append to local selection database"})
+                            
+    try:
+        with open(selection_history_database_dir, 'wb') as f:
+            pickle.dump(selection_history_database, f)
+    except:
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"msg" : "Cannot dump selection database!"})
+    
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={"msg" : "Selection History Database Updated!"})
+
+@router.delete("/diary_records")
+def clear_diary_db():
+    """ diary_history_database를 clear합니다.
+    """
+    global diary_history_database
+    diary_history_database = deque()
+    with open(diary_history_database_dir, 'wb') as f:
+            pickle.dump(diary_history_database, f)
+
+@router.delete("/selection_records")
+def clear_selection_db():
+    """ selection_history_database를 clear합니다.
+    """
+    global selection_history_database
+    selection_history_database = deque()
+    with open(selection_history_database_dir, 'wb') as f:
+            pickle.dump(selection_history_database, f)
