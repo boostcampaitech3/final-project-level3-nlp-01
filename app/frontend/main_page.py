75be6b18c6547e5a19a544dfb66bfed531443d51
leeyejin1231 <ssgyejin@g.skku.edu>
2022-06-03 08:59:36 +0000

    done!

--- app/frontend/main_page.py
+++ app/frontend/main_page.py
@@ -13,84 +13,6 @@ import base64  # 나중에 이미지 업로드 용
 # }
 
 
-def get_diary():
-    diary = requests.get(url="http://localhost:8000/history/diary")
-    diary_history = eval(diary.content.decode('UTF-8'))
-    return diary_history
-
-
-st.markdown('<p class="title">하루의 마침표.</p>', unsafe_allow_html=True)
-st.markdown('<p class="sub_title">오늘을 마무리하기 전, 당신의 감정에 맞는 컨텐츠를 소개해드립니다.</p>', unsafe_allow_html=True)
-
-_, col, _ = st.columns([2.5]*2+[1.18])
-write_button = col.button("일기 쓰러가기")
-
-# if write_button:
-#     page = PAGES['Write']
-#     page.app()
-
-diarys = get_diary()
-
-for diary_history in diarys:
-
-    diary_time = diary_history['record_time']
-    diary_content = diary_history['diary_content']
-    feelings = diary_history['feelings']
-    # recommended_contents = diary_history['recommended_contents']
-    # recom_con_song = recommended_contents["songs"]
-    # recom_con_ = recommended_contents["songs"]
-
-    print("\n\n\n=======diary_time : ", diary_time)
-    print("\n\n\n=======diary_content : ", diary_content)
-    print("\n\n\n=======feelings : ", feelings)
-    # print("\n\n\n=======recommended_contents : ", recommended_contents)
-
-    st.markdown("***", unsafe_allow_html=True)
-
-    st.markdown(f'<p class="content">{diary_content}</p>', unsafe_allow_html=True)
-    st.markdown(f'<p class="emotions">#{feelings[0]} #{feelings[1]} #{feelings[2]}</p>', unsafe_allow_html=True)
-    st.markdown(f'<p class="date">{diary_time}</p>', unsafe_allow_html=True)
-
-    st.markdown('<p class="recom">추천 콘텐츠</p>', unsafe_allow_html=True)
-    st.markdown('''<div class="box">
-        <div class="div2">
-            <img class="song_image" src="https://cdnimg.melon.co.kr/cm2/album/images/109/37/474/10937474_20220428225312_500.jpg/melon/resize/120/quality/80/optimize">
-            <div class="div1">
-                <a class="box_title" href="https://www.melon.com/song/detail.htm?songId=34997078" target="_blank">That That</a>
-                <p class="box_singer">싸이</p>
-                <p class="box_content">준비하시고 쏘세요 That That I like That 좌 우 위 아래로<br>That That I like That</p>
-            </div>
-        </div>
-        <div><p class="what">노래</p></div>
-    </div>''', unsafe_allow_html=True)
-
-st.markdown('''<div class="box">
-    <div class="div2">
-        <img class="song_image" src="https://cdnimg.melon.co.kr/cm2/album/images/109/37/474/10937474_20220428225312_500.jpg/melon/resize/120/quality/80/optimize">
-        <div class="div1">
-            <a class="box_title" href="https://www.melon.com/song/detail.htm?songId=34997078" target="_blank">That That</a>
-            <p class="box_singer">싸이</p>
-            <p class="box_content">준비하시고 쏘세요 That That I like That 좌 우 위 아래로<br>That That I like That</p>
-        </div>
-    </div>
-    <div><p class="what">노래</p></div>
-</div>''', unsafe_allow_html=True)
-
-st.markdown('''<div class="box">
-    <div class="div2">
-        <img class="movie_image" src="https://search.pstatic.net/common?type=o&size=174x242&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20220516_144%2F1652665409592Chvey_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2">
-        <div class="div1">
-            <a class="box_title" href="https://movie.naver.com/movie/bi/mi/basic.naver?code=192608" target="_blank">범죄도시</a>
-            <p class="box_content">가리봉동 소탕작전 후 4년 뒤, 금천서 강력반은 베트남으로 도주한 용의자를 인도받아 오라는 미션을 받는다. 괴물형사 ‘마석도’(마동석)와 ‘전일만’(최귀화) 반장은 현지 용의자에게서 수상함을 느끼고, 그의 뒤에 무자비한 악행을 벌이는 ‘강해상’(손석구)이 있음을 알게 된다. ‘마석도’와 금천서 강력반은 한국...</p>
-        </div>
-    </div>
-    <div><p class="what">영화</p></div>
-</div>''', unsafe_allow_html=True)
-
-
-
-
-
 ####### style
 st.markdown("""<style>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@300&family=Song+Myung&display=swap');
@@ -116,7 +38,7 @@ st.markdown("""<style>
 }
 .box_content{
     font-size: 16px;
-    margin: 5px;
+    margin-left: 5px;
 }
 .emotions{
     font-size: 20px;
@@ -130,14 +52,36 @@ st.markdown("""<style>
 .box_singer{
     font-size: 15px;
     color: grey;
-    margin: 5px;
+    margin-left: 5px;
+    margin-bottom: 5px;
 }
-.what{
+.what_music{
     font-size: 15px;
     color: #E2B79A;
     text-align: right;
     padding-right: 5px;
-    width: 40px;
+    width: 35px;
+}
+.what_book{
+    font-size: 15px;
+    color: #B38EB9;
+    text-align: right;
+    padding-right: 5px;
+    width: 35px;
+}
+.what_movie{
+    font-size: 15px;
+    color: #ECCB3F;
+    text-align: right;
+    padding-right: 5px;
+    width: 35px;
+}
+.what_play{
+    font-size: 15px;
+    color: #A5C7A1;
+    text-align: right;
+    padding-right: 5px;
+    width: 35px;
 }
 .box{
     box-sizing: border-box;
@@ -166,11 +110,11 @@ st.markdown("""<style>
 }
 .song_image{
     margin-right: 10px;
-    height: 110px;
+    height: 90px;
 }
 .movie_image{
     margin-right: 10px;
-    height: 130px;
+    height: 100px;
 }
 .recom{
     text-size: 20px;
@@ -179,3 +123,117 @@ st.markdown("""<style>
     margin-left: 10px;
 }
 </style>""", unsafe_allow_html=True)
+
+
+
+
+def get_diary():
+    diary = requests.get(url="http://localhost:8000/history/diary")
+    diary_history = eval(diary.content.decode('UTF-8'))
+    return diary_history
+
+
+st.markdown('<p class="title">하루의 마침표.</p>', unsafe_allow_html=True)
+st.markdown('<p class="sub_title">오늘을 마무리하기 전, 당신의 감정에 맞는 컨텐츠를 소개해드립니다.</p>', unsafe_allow_html=True)
+
+_, col, _ = st.columns([2.5]*2+[1.18])
+write_button = col.button("일기 쓰러가기")
+
+# if write_button:
+#     page = PAGES['Write']
+#     page.app()
+
+diarys = get_diary()
+
+for diary_history in diarys:
+
+    diary_time = diary_history['record_time']
+    diary_content = diary_history['diary_content']
+    feelings = diary_history['feelings']
+    recommended_contents = diary_history['recommended_content']
+    recom_con_songs = recommended_contents["songs"]
+    recom_con_books = recommended_contents["books"]
+    recom_con_movies = recommended_contents["movies"]
+    recom_con_plays = recommended_contents["plays"]
+
+    print("\n\n\n=======diary_time : ", diary_time)
+    print("\n\n\n=======diary_content : ", diary_content)
+    print("\n\n\n=======feelings : ", feelings)
+    print("\n\n\n=======song : ", recom_con_songs[0][0])
+
+    st.markdown("***", unsafe_allow_html=True)
+
+    st.markdown(f'<p class="content">{diary_content}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="emotions">#{feelings[0]} #{feelings[1]} #{feelings[2]}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p class="date">{diary_time}</p>', unsafe_allow_html=True)
+
+    st.markdown('<p class="recom">추천 콘텐츠</p>', unsafe_allow_html=True)
+    
+    # 노래
+    for song in recom_con_songs:
+        if len(song) == 0:
+            continue
+        st.markdown(f'''<div class="box">
+            <div class="div2">
+                <img class="song_image" src=https://cdnimg.melon.co.kr/cm2/album/images/109/37/474/10937474_20220428225312_500.jpg/melon/resize/120/quality/80/optimize">
+                <div class="div1">
+                    <a class="box_title" href={song[0]['hyperlink']} target="_blank">{song[0]['title']}</a>
+                    <p class="box_singer">{song[0]['singer']}</p>
+                    <p class="box_content">{song[0]['preview']}</p>
+                </div>
+            </div>
+            <div><p class="what_music">노래</p></div>
+        </div>''', unsafe_allow_html=True)
+
+    # 책
+    for book in recom_con_books:
+        if len(book) == 0:
+            continue
+        st.markdown(f'''<div class="box">
+            <div class="div2">
+                <img class="movie_image" src={book[0]['image']}>
+                <div class="div1">
+                    <a class="box_title" href={book[0]['hyperlink']} target="_blank">{book[0]['title']}</a>
+                    <p class="box_singer">{book[0]['author']}</p>
+                    <p class="box_content">{book[0]['preview']}</p>
+                </div>
+            </div>
+            <div><p class="what_book">책</p></div>
+        </div>''', unsafe_allow_html=True)
+
+    #영화
+    for movie in recom_con_movies:
+        if len(movie) == 0:
+            continue
+        st.markdown(f'''<div class="box">
+            <div class="div2">
+                <img class="movie_image" src={movie[0]['image']}>
+                <div class="div1">
+                    <a class="box_title" href={movie[0]['hyperlink']} target="_blank">{movie[0]['title']}</a>
+                    <p class="box_content">{movie[0]['preview']}</p>
+                </div>
+            </div>
+            <div><p class="what_movie">영화</p></div>
+        </div>''', unsafe_allow_html=True)
+
+    #연극
+    for play in recom_con_plays:
+        if len(play) == 0:
+            continue
+        st.markdown(f'''<div class="box">
+            <div class="div2">
+                <img class="movie_image" src={play[0]['image']}>
+                <div class="div2">
+                    <a class="box_title" href={play[0]['hyperlink']} target="_blank">{play[0]['title']}</a>
+                    <p class="box_content">{play[0]['preview']}</p>
+                </div>
+            </div>
+            <div><p class="what_play">연극</p></div>
+        </div>''', unsafe_allow_html=True)
+
+
+
+
+
+
+
