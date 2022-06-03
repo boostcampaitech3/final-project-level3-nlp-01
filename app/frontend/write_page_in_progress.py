f8b9cda137437b4ae709942697fdb1dfa62f3a08
Ellie J <hummingeel@gmail.com>
2022-06-03 14:09:34 +0900

    writing code in progress

--- app/frontend/write_page_in_progress.py
+++ app/frontend/write_page_in_progress.py
@@ -1,6 +1,8 @@
+from xmlrpc.client import Boolean
 import streamlit as st
 import requests
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+import random
 # import base64  # 나중에 이미지 업로드 용
 # from multiapp import MultiApp
 
@@ -68,15 +70,40 @@ user_label_dict = {}
 KOTE_label = ['불평/불만', '환영/호의', '감동/감탄', '지긋지긋', '고마움', '슬픔', '화남/분노', '존경', '기대감', '우쭐댐/무시함', '안타까움/실망', '비장함', '의심/불신', '뿌듯함', '편안/쾌적', '신기함/관심', '아껴주는', '부끄러움', '공포/무서움', '절망', '한심함', '역겨움/징그러움', '짜증', '어이없음', '없음', '패배/자기혐오', '귀찮음', '힘듦/지침', '즐거움/신남', '깨달음', '죄책감', '증오/혐오', '흐뭇함(귀여움/예쁨)', '당황/난처', '경악', '부담/안_내킴', '서러움', '재미없음', '불쌍함/연민', '놀람', '행복', '불안/걱정', '기쁨', '안심/신뢰']
 KOTE_label_dict = {i:KOTE_label[i] for i in range(len(KOTE_label))}
 
-## session state; avoiding refresing the entire page when clicking a button
-# 
-if 'user_button' not in st.session_state:
-    st.session_state.user_button = True
+# keys list for KOTE label
+keys_kote = [i for i in range(len(KOTE_label))]
 
-if 'user_button' not in st.session_state:
-    st.session_state.user_button = True
-# def update_session():
-#     st.session_state.update = 
+# keys list for top 3 label
+keys_top3 = random.sample(range(1000,9999), 3)
+
+## session state; avoiding refresing the entire page when clicking a button 
+# callback to update 'test' based on 'check'
+def flip1():
+    if st.session_state["check1"]:
+        st.session_state["test1"] = True
+    else:
+        st.session_state["test1"] = False
+
+def flip2():
+    if st.session_state["check2"]:
+        st.session_state["test2"] = True
+    else:
+        st.session_state["test2"] = False
+
+
+if "test1" not in st.session_state:
+    st.session_state["test1"] = True
+
+if "test2" not in st.session_state:
+    st.session_state["test2"] = True
+
+# filp test
+# st.checkbox(
+#     "Flip the switch", value=st.session_state["test"], key="check", on_change=flip
+# )
+
+# st.write(st.session_state["test"])
+
 
 def get_feelings_from_diary(user_diary: str) -> List:
     response = requests.post(url="http://localhost:8000/diary/input", json = {"diary_content": user_diary})
@@ -98,65 +125,131 @@ def select_emotion_label(temp_data: Tuple) -> List:
     return final_selection
 
 # @st.cache(suppress_st_warning=True)
-# def return_user_feelings() -> List:
-#     global emotions
-#     global user_label_dict
-#     # print("emotions from get_feelings_from_diary", emotions)
-#     emotions = get_feelings_from_diary(user_diary)
-#     # print(emotions, len(emotions), type(emotions))
+def return_user_feelings(user_feelings_button=False) -> List:
+    global emotions
+    global user_label_dict
 
-#     st.markdown('<p class="emotions">감정 분석 결과입니다!</p>', unsafe_allow_html=True)
+    _, col, _ = st.columns([1.5]*2+[1])
+    # user_feelings_button = col.checkbox("당신의 감정을 정리해드릴게요", value=st.session_state["test1"], key='check1', on_change=flip1)  # st.button은 session_state를 지원하지 않아서 임시방편으로 chckbox를 사용함
+    print("emotions from get_feelings_from_diary", emotions)
+    st.markdown("***", unsafe_allow_html=True)
+    emotions = get_feelings_from_diary(user_diary)
+    print(emotions, len(emotions), type(emotions))
  
-#     option1 = st.checkbox(emotions[0])
-#     option2 = st.checkbox(emotions[1])
-#     option3 = st.checkbox(emotions[2])
-#     index = [option1, option2, option3]
+    # 여기서 끊자-> return emotion를 
 
-#     return ("emotions", index)
+    st.markdown('<p class="emotions">감정 분석 결과입니다!</p>', unsafe_allow_html=True)
 
 
-global no_emotion
-no_emotion = False
+    _, col1, _ = st.columns([2.7]*2+[1])
+    _, col2, _ = st.columns([2.7]*2+[1])
+    _, col3, _ = st.columns([2.7]*2+[1])
+    #_, col4, _ = st.columns([2.7]*2+[1])
+    option1 = col1.checkbox(emotions[0])
+    option2 = col2.checkbox(emotions[1])
+    option3 = col3.checkbox(emotions[2])
+    _, column, _ = st.columns([2.7]*2+[1])
+    there_is_no_emotions = column.checkbox("원하는 감정이 없어요!", value=st.session_state["test2"], key='check2', on_change=flip2)
+    # there_is_no_emotions = col4.checkbox("원하는 감정이 없어요!", value=st.session_state["test2"], key=keys_top3[3], on_change=flip2)
+    
+    
+    if there_is_no_emotions:
+        st.markdown("***", unsafe_allow_html=True)
+        st.markdown('<p class="emotions">마음에 드는 감정이 없다면, 원하는 감정 없음 체크박스를 선택해주세요!</p>', unsafe_allow_html=True)
+        for i in range(len(KOTE_label)):
+            globals()[f'options_{i}'] = st.checkbox(KOTE_label[i])
+        index = [f'options_{i}' for i in range(len(KOTE_label))]
+        print(index)
+        return ("KOTE", index)
+    else:
+        index = [option1, option2, option3]
+        return ("emotions", index)
+    # else:
+    #     _, col2, _ = st.columns([2.7]*2+[1])
+    #         _, col3, _ = st.columns([2.7]*2+[1])
+    #         _, col4, _ = st.columns([2.7]*2+[1])
+    #         _, col5, _ = st.columns([2.7]*2+[1])
+    #         option1 = col2.checkbox(emotions[0])
+    #         option2 = col3.checkbox(emotions[1])
+    #         option3 = col4.checkbox(emotions[2])
+    #         index = [option1, option2, option3]
+
+    #         return ("emotions", index)
+
+    
+
+    # index = [option1, option2, option3]
+    # # there_is_no_emotions = st.radio(label="원하는 감정이 없다면 아래 radio를 선택하세요!", options=['원하는 감정이 없어요'], key='emotion_checkbox', disabled=False)
+    # there_is_no_emotions = st.checkbox("원하는 감정이 없어요!", value=st.session_state["test2"], key="check2", on_change=flip2) 
+    
+    # if there_is_no_emotions:
+    #     for i in range(len(KOTE_label)):
+    #         globals()[f'options_{i}'] = st.checkbox(KOTE_label[i])
+    #     index = [f'options_{i}' for i in range(len(KOTE_label))]
+    #     return ("KOTE", index)
+    # else:
+    #     _, col2, _ = st.columns([2.7]*2+[1])
+    #     _, col3, _ = st.columns([2.7]*2+[1])
+    #     _, col4, _ = st.columns([2.7]*2+[1])
+    #     _, col5, _ = st.columns([2.7]*2+[1])
+    #     option1 = col2.checkbox(emotions[0])
+    #     option2 = col3.checkbox(emotions[1])
+    #     option3 = col4.checkbox(emotions[2])
+    #     there_is_no_emotions = col5.checkbox("원하는 감정이 없어요!", value=st.session_state["test2"], key="check2", on_change=flip2) 
+
+    #     index = [option1, option2, option3]
+
+    # return ("emotions", index)
+
+
+# global no_emotion
+# no_emotion = False
 
 ###############UI
 st.markdown('<p class="title">하루의 마침표.</p>', unsafe_allow_html=True)
 st.markdown('<p class="sub_title">오늘을 마무리하기 전, 당신의 감정에 맞는 컨텐츠를 소개해드립니다.</p>', unsafe_allow_html=True)
 user_diary = st.text_area(label ="", placeholder = f"오늘 하루는 어떠셨나요? 일기든, 감정을 나타내는 키워드든 자유로운 형식으로 정리해보세요.", height=250)
-_, col, _ = st.columns([1]*2+[1])
-user_feelings_button = col.button("당신의 감정을 정리해드릴게요", key='user_button')  # st.button은 session_state를 지원하지 않아서 임시방편으로 chckbox를 사용함
+_, col, _ = st.columns([1.5]*2+[1])
+user_feelings_button = col.checkbox("당신의 감정을 정리해드릴게요", value=st.session_state["test1"], key='check1', on_change=flip1)   # st.button은 session_state를 지원하지 않아서 임시방편으로 chckbox를 사용함
 
 
-if user_feelings_button:
-    st.markdown("***", unsafe_allow_html=True)
-    emotions = get_feelings_from_diary(user_diary)
+### 여기서부터 테스트
+print("============================== final_selection check!")
+emotion_data = return_user_feelings(user_feelings_button)
+final_selection = select_emotion_label(emotion_data)
+print("fianl_selection: ", final_selection)
+print("=================================== Success!!")
+# if user_feelings_button:
+#     st.markdown("***", unsafe_allow_html=True)
+#     emotions = get_feelings_from_diary(user_diary)
 
-    st.markdown('<p class="emotions">감정 분석 결과입니다!</p>', unsafe_allow_html=True)
+#     st.markdown('<p class="emotions">감정 분석 결과입니다!</p>', unsafe_allow_html=True)
     
-    _, col2, _ = st.columns([2.7]*2+[1])
-    _, col3, _ = st.columns([2.7]*2+[1])
-    _, col4, _ = st.columns([2.7]*2+[1])
-    col2.checkbox(emotions[0])
-    col3.checkbox(emotions[1])
-    col4.checkbox(emotions[2])
+#     _, col2, _ = st.columns([2.7]*2+[1])
+#     _, col3, _ = st.columns([2.7]*2+[1])
+#     _, col4, _ = st.columns([2.7]*2+[1])
+#     col2.checkbox(emotions[0])
+#     col3.checkbox(emotions[1])
+#     col4.checkbox(emotions[2])
 
-    st.markdown('<p class="what">원하는 감정을 선택하세요</p>', unsafe_allow_html=True)
-    _, col5, _ = st.columns([2]*2+[1])
-    recom_button = col5.button("컨텐츠 추천 받기", key='recom_button')
+#     st.markdown('<p class="what">원하는 감정을 선택하세요</p>', unsafe_allow_html=True)
+#     _, col5, _ = st.columns([2]*2+[1])
+#     recom_button = col5.button("컨텐츠 추천 받기", key='recom_button')
 
-    st.markdown('<p class="what">원하는 감정이 없으신가요?</p>', unsafe_allow_html=True)
-    _, col6, _ = st.columns([1.5]*2+[1])
-    no_emotions_button = col6.button("원하는 감정이 없어요!", key='no_emo_button')
-    # option2 = st.checkbox(emotions[1])
-    # option3 = st.checkbox(emotions[2])
-    # index = [option1, option2, option3]
+#     st.markdown('<p class="what">원하는 감정이 없으신가요?</p>', unsafe_allow_html=True)
+#     _, col6, _ = st.columns([1.5]*2+[1])
+#     no_emotions_button = col6.button("원하는 감정이 없어요!", key='check', on_change = flip2)
+#     option2 = st.checkbox(emotions[1])
+#     option3 = st.checkbox(emotions[2])
+#     index = [option1, option2, option3]
 
     # there_is_no_emotions = st.button("원하는 감정이 없어요!",)
-    if no_emotions_button:
-        no_emotion = True
-        pass
+    # if no_emotions_button:
+    #     no_emotion = True
+    #     pass
 
-if no_emotion:
-        st.markdown('<p class="emotions">원하는 감정을 선택하세요!</p>', unsafe_allow_html=True)
+# if no_emotion:
+#         st.markdown('<p class="emotions">원하는 감정을 선택하세요!</p>', unsafe_allow_html=True)
 
 
 
