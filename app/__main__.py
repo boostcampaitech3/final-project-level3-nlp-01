d1a771112f3abe2e2f37b6161ad8644c968eda99
Kang-Beom-Seo <bumsur@hanmail.net>
2022-06-13 07:12:51 +0000

    argument_parser added

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,16 @@
 import uvicorn
-import os
+import argparse
+
 from app.backend.main import app
+
+parser = argparse.ArgumentParser(description='클라우드에 있는 DB 관련 정보를 받습니다.')
+parser.add_argument('--url', required=True, help="DB의 url")
+parser.add_argument('--port', required=True, help="DB의 port")
+parser.add_argument('--db_name', required=True, help="DB의 테이블 이름")
+args = parser.parse_args()
+URL = args.url
+PORT = int(args.port)
+DB_NAME = args.db_name
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-    # os.system("streamlit run frontend/write_page_in_progress.py --server.port 30001")
